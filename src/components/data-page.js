3c35a6d375188db610d21944dda676df016fd47c
raysantori <contato@raysantori.com>
2023-08-31 21:38:40 -0300

    feat. new pages redirection

--- src/components/data-page.js
+++ src/components/data-page.js
@@ -9,14 +9,8 @@ const feedbackAdress = 'https://g.page/r/CRgDqyPHAu7UEBM/review';
 const emailAdress = 'mailto:contato@carolina.eng.br';
 const whatsappAdress = 'https://wa.me/5566999691956';
 const instagramAdress = 'https://www.instagram.com/carolinngomes/';
-
-// social redirections
-var linksRedirections = [
-    feedbackAdress, 
-    emailAdress, 
-    whatsappAdress, 
-    instagramAdress
-];
+const linkedinAdress = 'https://www.linkedin.com/in/carolinngomes/';
+const githubAdress = 'https://github.com/carolinngomes';
 
 // footer
 licenseFooter = `. Todos os direitos reservados.`;
