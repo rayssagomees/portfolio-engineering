55befaea92d54bfa6e43cc2affe4a1118bfc39f9
Rayssa Gomes <rayssacgomess@gmail.com>
2024-03-15 13:46:57 -0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 var footer = document.createElement('footer');
 const dateFooter = new Date();
 const fullYearFooter = dateFooter.getFullYear();
-const contentFooter = `© ${fullYearFooter} ${titlePage}${licenseFooter}`;
+const contentFooter = `© ${fullYearFooter} ${previousTitlePage}${titlePage}${licenseFooter}`;
 footer.innerText = contentFooter;
 document.body.appendChild(footer);
