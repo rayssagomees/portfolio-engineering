e50bbbb86ce1d1d741749bde1db6b9b7ab48d28d
Rayssa Gomes <rayssacgomess@gmail.com>
2024-03-15 11:25:59 -0300

    Update social links

--- src/components/data-page.js
+++ src/components/data-page.js
@@ -8,9 +8,9 @@ subtitlePage = ' - Unindo ideias e pessoas';
 const feedbackAdress = 'https://g.page/r/CRgDqyPHAu7UEBM/review';
 const emailAdress = 'mailto:contato@carolina.eng.br';
 const whatsappAdress = 'https://wa.me/5566999691956';
-const instagramAdress = 'https://www.instagram.com/carolinngomes/';
-const linkedinAdress = 'https://www.linkedin.com/in/carolinngomes/';
-const githubAdress = 'https://github.com/carolinngomes';
+const instagramAdress = 'https://www.instagram.com/engcivil/';
+const linkedinAdress = 'https://www.linkedin.com/in/rayssagomees/';
+const githubAdress = 'https://github.com/rayssagomees';
 
 // footer
 licenseFooter = `. Todos os direitos reservados.`;
