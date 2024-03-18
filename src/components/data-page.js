7dbdd2a7d08b01c4d3dcdf6bc48e767cff3e3864
Rayssa Gomes <rayssacgomess@gmail.com>
2024-03-18 09:54:50 -0300

    Fix social links

--- src/components/data-page.js
+++ src/components/data-page.js
@@ -5,13 +5,19 @@ authorName = 'Rayssa Gomes';
 slogan = 'Unindo ideias e pessoas'; 
     // O melhor jeito de construir os seus sonhos
 cta = 'Vamos construir juntos?'; 
+previousTitlePage = '';
+subtitlePage = '';
 betweenLines = ' - ';
 
 // social links
-const websiteAddress = 'https://www.carolina.eng.br/';
+const domain = 'carolina.eng.br';
+const email = 'contato@carolina.eng.br';
+const phone = '66999691956';
+
+const websiteAddress = `https://www.${domain}/`;
 const feedbackAddress = 'https://g.page/r/CRgDqyPHAu7UEBM/review';
-const emailAddress = 'mailto:contato@carolina.eng.br';
-const whatsappAddress = 'https://wa.me/5566999691956';
+const emailAddress = `mailto:${email}`;
+const whatsappAddress = `https://wa.me/55${phone}`;
 const instagramAddress = 'https://www.instagram.com/engrayssagomes/';
 const linkedinAddress = 'https://www.linkedin.com/in/rayssagomees/';
 const githubAddress = 'https://github.com/rayssagomees';
