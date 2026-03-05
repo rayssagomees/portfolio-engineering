6dc077b5bcbe7f15969d26d77c2a8620b1ec2731
Rayssa Gomes <rayssacgomess@gmail.com>
2026-03-05 13:17:54 -0300

    fix design

---
+++ src/components/form-submission.js
@@ -0,0 +1,7 @@
+
+// Form submission
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+   e.preventDefault();
+   alert('Obrigado pelo contato! Retornaremos em breve.');
+   this.reset();
+});
